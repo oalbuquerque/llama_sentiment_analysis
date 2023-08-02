229210a38b7fc1704503a846d6729df7f7ab2d44
oalbuquerque <139914000+oalbuquerque@users.noreply.github.com>
2023-08-02 18:22:01 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -127,14 +127,9 @@ def main(
         Sentimento:positivo
         ###
         Tweet:"mas será mesmo que preciso saber criar inteligência artificial para criar um chatbot."
-        Sentimento:""
-        ###
         Tweet:"google emite 'sinal de alerta' após lançamento do chatgpt."
-        Sentimento:""
         Tweet:"pra parecer história de filme você vai ter que procurar em todas as redes sociais e apps de reconhecimento facial."
-        Sentimento:""
-        Tweet:"os chatbots podem entregar a seu cliente exatamente o que ele quer apenas batendo um papo com ele acesse."
-        Sentimento:"""
+        Tweet:"os chatbots podem entregar a seu cliente exatamente o que ele quer apenas batendo um papo com ele acesse.""""
     ]
     results = generator.generate(
         prompts, max_gen_len=256, temperature=temperature, top_p=top_p
