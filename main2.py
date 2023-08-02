9e2a9d46401df5eac9a210a0cc77691306c30ec1
oalbuquerque <139914000+oalbuquerque@users.noreply.github.com>
2023-08-02 17:41:02 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,9 +1,13 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-model_name = alpaca'
-path = '/mnt/database/llama-main/'
+model_name = '7B'
+path = '/home/gerontech/llama2/models/'
 checkpoint = path + model_name
+
+#model = LlamaForCausalLM.from_pretrained("/home/gerontech/llama2/models/7B") # PATH_TO_CONVERTED_TOKENIZER
+#tokenizer = LlamaTokenizer.from_pretrained("/home/gerontech/llama2/models/tokenizer.model") # PATH_TO_CONVERTED_WEIGHTS
+
 
 model = AutoModelForCausalLM.from_pretrained(checkpoint, device_map="auto")
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
