78187933787bfe995ef50d5ac96268187b8c685d
oalbuquerque <139914000+oalbuquerque@users.noreply.github.com>
2023-08-07 15:53:09 -0300

    Update chat_completion.py

--- chat_completion.py
+++ chat_completion.py
@@ -2,7 +2,7 @@
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
 from typing import Optional
-
+import torch
 import fire
 
 from llama import ModelArgs, Transformer, Tokenizer, Llama
