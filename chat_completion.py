9ef59471e26c5ea3b7c8623e2f6bc77e5ec03b9c
oalbuquerque <139914000+oalbuquerque@users.noreply.github.com>
2023-08-07 16:17:43 -0300

    Update chat_completion.py

--- chat_completion.py
+++ chat_completion.py
@@ -1,21 +1,50 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
-from typing import Optional
+from typing import Tuple
+import os
+import sys
 import torch
 import fire
 import time
+import json
+import pandas as pd
+
 from pathlib import Path
-from llama import LLaMA
+
+from fairscale.nn.model_parallel.initialize import initialize_model_parallel
+
+from llama import ModelArgs, Transformer, Tokenizer, LLaMA
+
+
+def setup_model_parallel() -> Tuple[int, int]:
+    local_rank = int(os.environ.get("LOCAL_RANK", -1))
+    world_size = int(os.environ.get("WORLD_SIZE", -1))
+
+    torch.distributed.init_process_group("nccl")
+    initialize_model_parallel(world_size)
+    torch.cuda.set_device(local_rank)
+
+    # seed must be the same in all processes
+    torch.manual_seed(1)
+    return local_rank, world_size
+
 
 def load(
     ckpt_dir: str,
     tokenizer_path: str,
+    local_rank: int,
+    world_size: int,
     max_seq_len: int,
     max_batch_size: int,
 ) -> LLaMA:
     start_time = time.time()
     checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
+    assert world_size == len(
+        checkpoints
+    ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is {world_size}"
+    ckpt_path = checkpoints[local_rank]
+    print("Loading")
     checkpoint = torch.load(ckpt_path, map_location="cpu")
     with open(Path(ckpt_dir) / "params.json", "r") as f:
         params = json.loads(f.read())
