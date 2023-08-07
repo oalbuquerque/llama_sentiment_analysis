f42aa95295aa7bedf931449c53ae3e49d8697e14
oalbuquerque <139914000+oalbuquerque@users.noreply.github.com>
2023-08-07 17:08:55 -0300

    Update chat_completion2.py

--- chat_completion2.py
+++ chat_completion2.py
@@ -17,7 +17,7 @@ def main(
     max_batch_size: int = 4,
     max_gen_len: Optional[int] = None,
 ):
-    generator = LLaMA.build(
+    generator = Llama.build(
         ckpt_dir=ckpt_dir,
         tokenizer_path=tokenizer_path,
         max_seq_len=max_seq_len,
