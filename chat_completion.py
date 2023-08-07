804e2270faae634a972e08bd335bf6b763f3364a
oalbuquerque <139914000+oalbuquerque@users.noreply.github.com>
2023-08-07 16:24:26 -0300

    Update chat_completion.py

--- chat_completion.py
+++ chat_completion.py
@@ -73,7 +73,7 @@ def main(
     max_gen_len: int = None,
 ):
     generator = load(
-        cckpt_dir, tokenizer_path, local_rank, world_size, max_seq_len, max_batch_size
+        ckpt_dir, tokenizer_path, local_rank, world_size, max_seq_len, max_batch_size
     )
 
     dialogs = [
