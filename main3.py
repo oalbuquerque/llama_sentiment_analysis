70889ace38398c98251a7ec28e645139b4be151e
oalbuquerque <139914000+oalbuquerque@users.noreply.github.com>
2023-08-07 19:19:38 -0300

    Update main3.py

--- main3.py
+++ main3.py
@@ -27,7 +27,7 @@ def main(
     top_p: float = 0.9,
     max_seq_len: int = 1512,
     max_batch_size: int = 8,
-    max_gen_len: Optional[int] = 512,
+    #max_gen_len: Optional[int] = 512,
 ):
     generator = Llama.build(
         ckpt_dir=ckpt_dir,
